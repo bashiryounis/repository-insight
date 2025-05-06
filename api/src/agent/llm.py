79697dfd05efe2d6176efc5e2c34d7f875d8d697
younis3 <younis@omicmd.com>
2025-05-06 14:50:01 +0300

    - support gpt llm

--- api/src/agent/llm.py
+++ api/src/agent/llm.py
@@ -1,4 +1,6 @@
 from src.core.config import config 
 from llama_index.llms.google_genai import GoogleGenAI
+from llama_index.llms.openai import OpenAI
 
 llm_gemini = GoogleGenAI(model="gemini-2.0-flash", api_key=config.GOOGLE_API_KEY)
+llm_open= OpenAI(model="gpt-4o-mini", api_key=config.OPENAI_API_KEY)
