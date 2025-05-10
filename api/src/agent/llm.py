ded8549d2fb97219ed690360a1d97a595396f88c
younis3 <younis@omicmd.com>
2025-05-10 11:29:26 +0300

    - add gemini pro model

--- api/src/agent/llm.py
+++ api/src/agent/llm.py
@@ -3,4 +3,5 @@ from llama_index.llms.google_genai import GoogleGenAI
 from llama_index.llms.openai import OpenAI
 
 llm_gemini = GoogleGenAI(model="gemini-2.0-flash", api_key=config.GOOGLE_API_KEY)
+llm_gemini_pro = GoogleGenAI(model="gemini-1.5-pro", api_key=config.GOOGLE_API_KEY)
 llm_open= OpenAI(model="gpt-4o-mini", api_key=config.OPENAI_API_KEY)
