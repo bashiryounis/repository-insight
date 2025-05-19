c287629dbd2d0d24d5115f56b35e1b26abb688cd
younis3 <younis@omicmd.com>
2025-05-19 15:50:39 +0300

    - refactor llm as function calling

--- api/src/agent/llm.py
+++ api/src/agent/llm.py
@@ -1,7 +1,10 @@
 from src.core.config import config 
-from llama_index.llms.google_genai import GoogleGenAI
-from llama_index.llms.openai import OpenAI
 
-llm_gemini = GoogleGenAI(model="gemini-2.0-flash", api_key=config.GOOGLE_API_KEY)
-llm_gemini_pro = GoogleGenAI(model="gemini-1.5-pro", api_key=config.GOOGLE_API_KEY)
-llm_open= OpenAI(model="gpt-4o-mini", api_key=config.OPENAI_API_KEY)
+def get_llm_gemini(pro:bool = False):
+    from llama_index.llms.google_genai import GoogleGenAI
+    if pro : 
+        GoogleGenAI(model="gemini-1.5-pro", api_key=config.GOOGLE_API_KEY)
+    return GoogleGenAI(model="gemini-2.0-flash", api_key=config.GOOGLE_API_KEY)
+def get_llm_openai():
+    from llama_index.llms.openai import OpenAI
+    return OpenAI(model="gpt-4o-mini", api_key=config.OPENAI_API_KEY)
